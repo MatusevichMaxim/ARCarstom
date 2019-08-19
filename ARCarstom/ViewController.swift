05a8f1a833865e088017f85086aff6c4b0736223
Maxim <imulir@gmail.com>
2019-08-19 13:38:01 +0300

    Removed plane

--- ARCarstom/ViewController.swift
+++ ARCarstom/ViewController.swift
@@ -31,7 +31,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     var screenCenter: CGPoint?
     
     var rimScene = RimScene()
-//    var planeNode: SCNNode?
     
     
     // MARK: initialization
@@ -88,61 +87,19 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         sceneView.session.run(sessionConfig, options: [.resetTracking, .removeExistingAnchors])
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        DispatchQueue.main.async {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                self.addPlane(node: node, anchor: planeAnchor)
-            }
-        }
-    }
-    
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         DispatchQueue.global().async() {
             self.updateCoreML(with: frame)
         }
     }
     
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        guard let anchorPlane = anchor as? ARPlaneAnchor else { return }
-        
-//        planeNode?.position = SCNVector3(anchorPlane.center.x, anchorPlane.center.y, anchorPlane.center.z)
-    }
-    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if touches.first != nil {
             
-            let gridMaterial = SCNMaterial()
-            gridMaterial.diffuse.contents = UIColor.white.withAlphaComponent(0.0)
-//            planeNode?.geometry?.materials = [gridMaterial]
             sceneView.scene.rootNode.addChildNode(rimScene)
             
             rimAdded = true
         }
-    }
-    
-    // MARK: - Planes
-    
-    func addPlane(node: SCNNode, anchor: ARPlaneAnchor) {
-        
-        let pos = SCNVector3.positionFromTransform(anchor.transform)
-        print("NEW SURFACE DETECTED AT \(pos.friendlyString())")
-        
-        let plane = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
-        
-        let gridMaterial = SCNMaterial()
-        gridMaterial.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
-        plane.materials = [gridMaterial]
-
-//        planeNode = SCNNode()
-//        planeNode?.name = planeName
-//        planeNode?.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
-//        planeNode?.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
-//        planeNode?.geometry = plane
-//        node.addChildNode(planeNode!)
-    }
-    
-    func updatePlane(anchor: ARPlaneAnchor) {
-//        planeNode?.position = SCNVector3Make(anchor.center.x, anchor.center.y, anchor.center.z)
     }
     
     func removeNode(named: String) {
@@ -406,79 +363,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         else if gesture.direction == .down {
             print("Swipe Down")
         }
-    }
-    
-    // MARK: Convertion
-    
-    var dragOnInfinitePlanesEnabled = false
-    
-    func worldPositionFromScreenPosition(_ position: CGPoint,
-                                         objectPos: SCNVector3?,
-                                         infinitePlane: Bool = false) -> (position: SCNVector3?, planeAnchor: ARPlaneAnchor?, hitAPlane: Bool) {
-        
-        // -------------------------------------------------------------------------------
-        // 1. Always do a hit test against exisiting plane anchors first.
-        //    (If any such anchors exist & only within their extents.)
-        
-        let planeHitTestResults = sceneView.hitTest(position, types: .existingPlaneUsingExtent)
-        if let result = planeHitTestResults.first {
-            
-            let planeHitTestPosition = SCNVector3.positionFromTransform(result.worldTransform)
-            let planeAnchor = result.anchor
-            
-            // Return immediately - this is the best possible outcome.
-            return (planeHitTestPosition, planeAnchor as? ARPlaneAnchor, true)
-        }
-        
-        // -------------------------------------------------------------------------------
-        // 2. Collect more information about the environment by hit testing against
-        //    the feature point cloud, but do not return the result yet.
-        
-        var featureHitTestPosition: SCNVector3?
-        var highQualityFeatureHitTestResult = false
-        
-        let highQualityfeatureHitTestResults = sceneView.hitTestWithFeatures(position, coneOpeningAngleInDegrees: 18, minDistance: 0.2, maxDistance: 2.0)
-        
-        if !highQualityfeatureHitTestResults.isEmpty {
-            let result = highQualityfeatureHitTestResults[0]
-            featureHitTestPosition = result.position
-            highQualityFeatureHitTestResult = true
-        }
-        
-        // -------------------------------------------------------------------------------
-        // 3. If desired or necessary (no good feature hit test result): Hit test
-        //    against an infinite, horizontal plane (ignoring the real world).
-        
-        if (infinitePlane && dragOnInfinitePlanesEnabled) || !highQualityFeatureHitTestResult {
-            
-            let pointOnPlane = objectPos ?? SCNVector3Zero
-            
-            let pointOnInfinitePlane = sceneView.hitTestWithInfiniteHorizontalPlane(position, pointOnPlane)
-            if pointOnInfinitePlane != nil {
-                return (pointOnInfinitePlane, nil, true)
-            }
-        }
-        
-        // -------------------------------------------------------------------------------
-        // 4. If available, return the result of the hit test against high quality
-        //    features if the hit tests against infinite planes were skipped or no
-        //    infinite plane was hit.
-        
-        if highQualityFeatureHitTestResult {
-            return (featureHitTestPosition, nil, false)
-        }
-        
-        // -------------------------------------------------------------------------------
-        // 5. As a last resort, perform a second, unfiltered hit test against features.
-        //    If there are no features in the scene, the result returned here will be nil.
-        
-        let unfilteredFeatureHitTestResults = sceneView.hitTestWithFeatures(position)
-        if !unfilteredFeatureHitTestResults.isEmpty {
-            let result = unfilteredFeatureHitTestResults[0]
-            return (result.position, nil, false)
-        }
-        
-        return (nil, nil, false)
     }
     
     // MARK: Empty session functions
